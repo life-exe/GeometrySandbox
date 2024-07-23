1177667abc35e23d173133ee34c67d66516a9378
Life EXE <lifeexe.art@gmail.com>
2024-07-23 11:14:53 +0300

    Bump version to 5.4

---
+++ generate_project_files_5.4.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem Set all paths
+SET EnginePath=c:\Program Files\Epic Games\UE_5.4
+SET BuildToolRelativePath=Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe
+SET VersionSelector=c:\Program Files (x86)\Epic Games\Launcher\Engine\Binaries\Win64\UnrealVersionSelector.exe
+SET ProjectName=GeometrySandbox.uproject
+
+devops/generate_project_files.bat "%EnginePath%" "%BuildToolRelativePath%" "%VersionSelector%" "%ProjectName%"
